56ec1123d174e319e14302744ba7f292e31cca65
Gabriel Garbugio <augusto1793bu@gmail.com>
2025-09-26 16:25:05 -0300

    Add logo and web manifest for WorkaPool application
    - Added logo512.png to the public directory for application branding.
    - Created site.webmanifest file to define application metadata, including icons and display settings.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes";
 import './styles/tailwind.css'; 
+// import { registerSw } from "virtual:pwa-register";
+
+// registerSw(); 
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
