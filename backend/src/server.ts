0e4f5e1616cabbab391fabd0c74429711da22857
Gabriel Garbugio <augusto1793bu@gmail.com>
2026-05-04 07:09:07 -0300

    apenas para voltar pra main

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,8 @@ import { WatchdogScheduler } from "./schedulers/watchdog/WatchdogScheduler";
 console.log("🧪 DATABASE_URL carregado:", process.env.DATABASE_URL);
 
 // Iniciar Watchdog Scheduler
-const watchdog = new WatchdogScheduler();
-watchdog.start();
+// const watchdog = new WatchdogScheduler();
+// watchdog.start();
 
 // Iniciar servidor
 const PORT = Number(process.env.PORT) || 3005; 
@@ -17,6 +17,6 @@ app.listen(PORT, "0.0.0.0", () => {
     `🚀 Backend rodando em modo ${process.env.NODE_ENV || "development"}`
   );
   console.log(`📡 Servidor: ${process.env.API_SERVER_URL + ":" + process.env.PORT}`);
-  console.log(`📚 Swagger: ${process.env.API_SERVER_URL + ":" + process.env.PORT + "/api-docs"}`);
+  console.log(`📚 Swagger: ${process.env.API_SERVER_URL + ":" + process.env.PORT + "/api/docs"}`);
   console.log(`❤️  Health Check: ${process.env.API_SERVER_URL + ":" + process.env.PORT + "/health"}`);
 });
