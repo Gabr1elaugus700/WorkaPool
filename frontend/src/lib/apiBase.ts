999a3d9d9757b625641a9e7ecf19c07f7ee7c953
Gabriel Garbugio <augusto1793bu@gmail.com>
2026-08-25 08:53:38 -0300

    chore: add .dockerignore and update output directory in vite.config.ts
    - Created a .dockerignore file to exclude unnecessary files and directories from Docker builds, improving build efficiency.
    - Updated the output directory in vite.config.ts from "dist-test" to "dist" for production readiness.
    - Adjusted API base URL in apiBase.ts to reflect the new port for the backend service.

--- frontend/src/lib/apiBase.ts
+++ frontend/src/lib/apiBase.ts
@@ -3,9 +3,9 @@ import axios from 'axios';
 // Função para obter a URL base dinamicamente
 export const getBaseUrl = (): string => {
    if (window.location.hostname === 'pooltecnica.no-ip.biz') {
-    return 'http://pooltecnica.no-ip.biz:3005';
+    return 'http://pooltecnica.no-ip.biz:3030';
   }
-  return import.meta.env.VITE_API_BASE_URL || 'http://192.168.0.32:3005';
+  return import.meta.env.VITE_API_BASE_URL || 'http://192.168.0.32:3030';
 };
 
 // Usar variável de ambiente com fallback
